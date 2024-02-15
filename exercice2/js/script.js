f04db550a199bf2341a53605938e0fe67e4a9119
Allan <allan.delatuillerie@gmail.com>
2024-02-15 16:36:26 +0100

    FINI!

--- exercice2/js/script.js
+++ exercice2/js/script.js
@@ -5,13 +5,19 @@ function playGame(){
     let baliseNumber = document.getElementById("givenNumber");
     let baliseMysteryNumber = document.getElementById("mysteryNumber");
     let myButton = document.getElementById("button");
+    let reloadButton = document.getElementById("reload");
     let tryNumber = 0
+
+    reloadButton.addEventListener("click", () =>{
+        window.location.reload()
+    })
 
     myButton.addEventListener("click", () => {
         let mysteryNumber = baliseMysteryNumber.value
         let givenNumber = baliseNumber.value
         tryNumber ++
         searchMysteryNumber(givenNumber, mysteryNumber, tryNumber)
+        ruleVerify(givenNumber, mysteryNumber)
         displayTry(tryNumber)
     })
 }
@@ -24,20 +30,14 @@ function playGame(){
  * @param {number} tryNumber : nombre de tentative du joueur 2
  */
 function searchMysteryNumber(givenNumber, mysteryNumber, tryNumber){
-    if (givenNumber < 1 || mysteryNumber < 1){
-        alert("Veuillez respecter les régles ! Un nombre entre 1 et 50")
-    }
-    else if(givenNumber > 50 || mysteryNumber > 50){
-        alert("Veuillez respecter les régles ! Un nombre entre 1 et 50")
-    }
-    else if(givenNumber < mysteryNumber){
+    if(givenNumber < mysteryNumber){
         displayResult("Votre nombre est trop petit  !")
     }
     else if (givenNumber > mysteryNumber){
         displayResult("Votre nombre est trop grand !")
     }
     else {
-        displayResult("Bravo ! Vous avez trouvez le nombre mystère en " + tryNumber + " tentatives !")       
+        displayResult("Bravo ! Vous avez trouvez le nombre mystère en " + tryNumber + " tentatives !")
     }
 }
 
@@ -57,4 +57,22 @@ function displayResult(resultGame){
 function displayTry(tryNumber){
     let baliseTry = document.getElementById("try")
     baliseTry.innerText = "Tentative n°" + tryNumber
+}
+
+/**
+ * Cette fonction compare le nombre recu avec le nombre mystere
+ * et vérifie s'ils respect la condition de 1 à 50
+ * @param {number} givenNumber : nombre choisi par le joueur 2
+ * @param {number} mysteryNumber : nombre choisi par le joueur 1
+ */
+function ruleVerify (givenNumber, mysteryNumber){
+    if (givenNumber < 1 || mysteryNumber < 1){
+        alert("Veuillez respecter les régles ! Un nombre entre 1 et 50")
+        window.location.reload()
+    }
+    else if (givenNumber > 50 || mysteryNumber > 50){
+        alert("Veuillez respecter les régles ! Un nombre entre 1 et 50")
+        window.location.reload()
+    }
+    else{}
 }
