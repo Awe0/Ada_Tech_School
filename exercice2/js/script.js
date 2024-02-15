1e2d9187eb0600405145365cbced2c070b95350d
Allan <allan.delatuillerie@gmail.com>
2024-02-15 11:51:04 +0100

    Poursuite de l'exo

--- exercice2/js/script.js
+++ exercice2/js/script.js
@@ -1,4 +1,23 @@
-function getValue(){
-    let givenNumber = document.getElementById("givenNumber").value;
+function playGame(){
+    let baliseNumber = document.getElementById("givenNumber");
     console.log(givenNumber);
+    let myButton = document.getElementById("button");
+    myButton.addEventListener("click", () => {
+        let givenNumber = baliseNumber.value
+        searchMysteryNumber(givenNumber)
+    })
+}
+
+function searchMysteryNumber(givenNumber){
+    let mysteryNumber = 22
+    console.log(givenNumber);
+    if(givenNumber == mysteryNumber){
+        console.log("Bravo");
+        displayResult("Bravo !")       
+    }
+}
+
+function displayResult(resultGame){
+    let result = document.getElementById("result")
+    result = resultGame
 }
