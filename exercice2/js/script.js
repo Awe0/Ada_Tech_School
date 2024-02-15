e9f5258294d9ef892300a0acdb1c53bdd57aa755
Allan <allan.delatuillerie@gmail.com>
2024-02-15 15:02:11 +0100

    Exercice fini !

--- exercice2/js/script.js
+++ exercice2/js/script.js
@@ -5,39 +5,56 @@ function playGame(){
     let baliseNumber = document.getElementById("givenNumber");
     let baliseMysteryNumber = document.getElementById("mysteryNumber");
     let myButton = document.getElementById("button");
+    let tryNumber = 0
+
     myButton.addEventListener("click", () => {
         let mysteryNumber = baliseMysteryNumber.value
         let givenNumber = baliseNumber.value
-        console.log(mysteryNumber);
-        console.log(givenNumber);
-        searchMysteryNumber(givenNumber, mysteryNumber)
+        tryNumber ++
+        searchMysteryNumber(givenNumber, mysteryNumber, tryNumber)
+        displayTry(tryNumber)
     })
 }
 
 /**
  * Cette fonction compare le nombre recu avec le nombre mystere
+ * et vérifie s'ils respect la condition de 1 à 50
  * @param {number} givenNumber : nombre choisi par le joueur 2
  * @param {number} mysteryNumber : nombre choisi par le joueur 1
+ * @param {number} tryNumber : nombre de tentative du joueur 2
  */
-function searchMysteryNumber(givenNumber, mysteryNumber){
-    if(givenNumber == mysteryNumber){
-        displayResult("Bravo !")       
+function searchMysteryNumber(givenNumber, mysteryNumber, tryNumber){
+    if (givenNumber < 1 || mysteryNumber < 1){
+        alert("Veuillez respecter les régles ! Un nombre entre 1 et 50")
+    }
+    else if(givenNumber > 50 || mysteryNumber > 50){
+        alert("Veuillez respecter les régles ! Un nombre entre 1 et 50")
+    }
+    else if(givenNumber < mysteryNumber){
+        displayResult("Votre nombre est trop petit  !")
     }
     else if (givenNumber > mysteryNumber){
-        displayResult("Trop grand !")
+        displayResult("Votre nombre est trop grand !")
     }
     else {
-        displayResult("Trop petit  !")
+        displayResult("Bravo ! Vous avez trouvez le nombre mystère en " + tryNumber + " tentatives !")       
     }
 }
 
 /**
- * Cette fonction affiche le resultat 
+ * Cette fonction affiche le resultat
  * @param {string} resultGame : texte envoyé par la fonction searchMysteryNumber
  */
 function displayResult(resultGame){
-    console.log(resultGame);
     let result = document.getElementById("result")
-    console.log(result);
-    result.innerText = resultGame
+    result.innerText = resultGame    
+}
+
+/**
+ * Cette fonction affiche le nombre de tentative
+ * @param {number} tryNumber 
+ */
+function displayTry(tryNumber){
+    let baliseTry = document.getElementById("try")
+    baliseTry.innerText = "Tentative n°" + tryNumber
 }
