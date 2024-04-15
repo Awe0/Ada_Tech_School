3f7c78d0f90dbbe8ba1a3dce320aa2a6597888fb
Allan <allan.delatuillerie@gmail.com>
2024-04-15 15:48:50 +0200

    Exercice fait en CSharp

---
+++ exercice2/Csharp/Exercice2/ClassesLib/Player.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassesLib;
+
+public class Player (int mysteryNumber, int minRange, int maxRange)
+{
+    private int givenNumber { get; set; }
+    private int tryNumber = 0;
+
+    public void askNumber()
+    {
+        System.Console.WriteLine("Entrez un nombre : ");
+        givenNumber = int.Parse(Console.ReadLine());
+        tryNumber++;
+        didIWin();
+    }
+    public void didIWin()
+    {
+        if (givenNumber < mysteryNumber)
+        {
+            minRange = givenNumber;
+            System.Console.WriteLine("Votre nombre se situe entre " + minRange + " et " + maxRange + "\nNombre de tentative = " + tryNumber);
+            askNumber();
+        }
+        else if (givenNumber > mysteryNumber)
+        {
+            maxRange = givenNumber;
+            System.Console.WriteLine("Votre nombre se situe entre " + minRange + " et " + maxRange + "\nNombre de tentative = " + tryNumber);
+            askNumber();
+        }
+        else
+        {
+            System.Console.WriteLine("Bravo, vous avez deviné le nombre !\nNombre de tentative = " + tryNumber);
+        }
+    }
+}
---
+++ exercice2/Csharp/Exercice2/Program.cs
@@ -0,0 +1,9 @@
+﻿using ClassesLib;
+
+int maxRange = 100;
+int minRange = 0;
+
+Random mysteryNumber = new Random();
+Player player = new Player(mysteryNumber.Next(minRange,maxRange), minRange, maxRange);
+
+player.askNumber();
